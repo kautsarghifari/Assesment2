7ae49c329e0f18ff6b8085cddd409375ae64e958
Ghifari <ghifarikautsar8484@gmail.com>
2024-05-06 09:39:49 +0700

    v1

---
+++ quiz.php
@@ -0,0 +1,31 @@
+<?php
+// Koneksi ke database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "quiz";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query untuk mengambil data dari tabel
+$sql = "SELECT * FROM quiz";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Mengubah data menjadi array assosiatif
+    $data = array();
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+
+    // Mengubah array menjadi format JSON
+    $json_data = json_encode($data);
+    echo $json_data;
+} else {
+    echo "No data found";
+}
+$conn->close();
+?>
